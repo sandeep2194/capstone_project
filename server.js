00ea2b4926d3ed1ab17c6e180afdbd64889e0b13
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-04-21 02:42:36 +0530

    done

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
-// Require Express to run server and routes
-
+projectData = [];
+// Express to run server and routes
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
+/* Dependencies */
+const bodyparser = require('body-parser');
 /* Middleware*/
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
+// Spin up the server
+const port = 3000
+const server = app.listen(port, callback());
+// Callback to debug
+function callback() {
+    console.log(`Server is running on ${port}`);
+}
+// Initialize all route with a callback function
+app.get('/all', callbackget);
+// Callback function to complete GET '/all'
+function callbackget(req, res) {
+    console.log('data sent')
+    res.send(projectData);
+};
+// Post Route
 
+app.post('/add', cb_post);
 
-// Setup Server
+function cb_post(req, res) {
+    const newData = req.body;
+    const newEntry = {
+        coord: newData.coord,
+        temp: newData.main.temp,
+        feels_like: newData.main.feels_like,
+        temp_min: newData.main.temp_min,
+        temp_max: newData.main.temp_max,
+        pressure: newData.main.pressure,
+        humidity: newData.main.humidity,
+    }
+    projectData.push(newEntry);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,70 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const baseurl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apikey = '&appid=c1373afd576221e460f658492a0f69fa';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', perform_magic)
+    /* Function called by event listener */
+function perform_magic() {
+    let zipcode = document.getElementById('zip').value;
+    getweather(baseurl, zipcode, apikey)
+        .then(function(data) {
+            postData("/add", data);
+        })
+        .then(
+            updateUI()
+        )
+}
+/* Function to GET Web API Data*/
+const getweather = async(baseurl, zipcode, apikey) => {
+    const res = await fetch(baseurl + zipcode + apikey)
+
+    try {
+        const data = await res.json();
+        return data;
+    } catch (e) {
+        console.log("error", e);
+    }
+}
+
+
+/* Function to POST data */
+const postData = async(url = " ", data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData
+
+    } catch (e) {
+        console.log("error", e);
+        // appropriately handle the error
+    }
+}
+
+/* Function to GET Project Data */
+
+const updateUI = async() => {
+    const response = await fetch('/all');
+    let d = new Date();
+    let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+    const date = document.getElementById('date');
+    const temp = document.getElementById('temp');
+    const content = document.getElementById('content');
+    try {
+        const allData = await response.json();
+        date.innerHTML = `<hr> Today's Date = ${newDate}`;
+        temp.innerHTML = `<br>Temprature today is ${allData["0"].temp} Ferenhite but it feels like ${allData["0"].feels_like} Ferenhite.`;
+        content.innerHTML = `<br>User's Feelings : ${document.getElementById('feelings').value}`;
+
+    } catch (e) {
+        console.log('erroe', e);
+    }
+}
