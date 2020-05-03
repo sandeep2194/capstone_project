9a1153bfc2d4732cbf5ea832c29d406eaf291fa5
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-05-04 01:05:23 +0530

    all three api done

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
 // Start up an instance of app
 const app = express();
@@ -16,6 +13,9 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('../../dist'));
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+
 // Spin up the server
 const port = 8081
 const server = app.listen(port, callback());
@@ -24,49 +24,75 @@ function callback() {
     console.log(`Server is running on ${port}`);
 }
 
-let weather_bit_api_key = process.env.weather_bit_api
-
+let all_data = {}
+let client_data = {};
 
 const getdata = async(baseurl) => {
     const res = await fetch(baseurl)
 
     try {
         const data = await res.json();
-        console.log(data)
         return data;
     } catch (e) {
         console.log("error", e);
     }
 };
-let geoname_data = {};
-let weatherbit_data = {};
-let client_data = {};
 
-function geoname(query) {
+const geoname = async(query) => {
     const baseurl = `http://api.geonames.org/searchJSON?q=${query}&maxRows=1&username=sandeep2194`;
-    getdata(baseurl).then((data) => {
-        weatherbit_data = data;
-    });
+    try {
+        await getdata(baseurl).then((data) => {
+            all_data['geoname_d'] = data.geonames[0];
+        })
+    } catch (e) {
+        console.log("error", e);
+    }
 };
 
-function weatherbit(query) {
-    const baseurl = `http://api.weatherbit.io/v2.0/forecast/days=${client_data.days_to_travelday}&key=${weather_bit_api_key}`;
-    getdata(baseurl).then((data) => {
-        geoname_data = data;
-    });
+const weatherbit = async(city, days_to_travelday) => {
+    const weather_bit_api_key = process.env.weather_bit_api
+    const baseurl = `http://api.weatherbit.io/v2.0/forecast/daily/?days=${days_to_travelday}&city=${city}&key=${weather_bit_api_key}`;
+    try {
+        await getdata(baseurl).then((data) => {
+            all_data['weather_bit_data'] = data.data[days_to_travelday - 1];
+        })
+    } catch (e) {
+        console.log("error", e);
+    }
 };
+
+const pixabay = async(city) => {
+    const pixabay_api_key = process.env.pixabay_api
+    const baseurl = `https://pixabay.com/api/?key=${pixabay_api_key}&q=${city}&image_type="photo"`;
+    try {
+        await getdata(baseurl).then((data) => {
+            all_data['pixabay_image'] = data.hits[0].webformatURL;
+        })
+    } catch (e) {
+        console.log("error", e);
+    }
+};
+
+
 app.get('/all', callbackget);
 // Callback function to complete GET '/all'
 function callbackget(req, res) {
-    res.send(geoname_data);
+    res.send(all_data)
     res.end()
 };
 
 app.post('/add', cb_post);
 
-
 function cb_post(req, res) {
     client_data = req.body;
-    console.log('The City is ' + city)
-    geoname(client_data.city);
+    let city = client_data.city;
+    let days_to_travel = client_data.days_to_travelday;
+    geoname(city)
+    weatherbit(city, days_to_travel)
+    pixabay(city).then(() => {
+        console.log(all_data)
+    })
+    res.send({})
+    res.end();
+
 };
