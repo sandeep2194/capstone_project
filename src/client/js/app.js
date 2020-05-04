65e035cf2334f9ad332ef7160d9a993a9445d152
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-05-04 06:36:06 +0530

    html done

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,14 +3,19 @@ import { postData } from "./helpers";
 import { date_diff_indays } from "./helpers";
 
 let all_data = {}
+let c_data = {}
 
 function magic() {
     const city = document.getElementById('city').value
     geoname(city).then(() => {
         weatherbit(city).then(() => {
             pixabay(city).then(() => {
-                postData(backend_callback_url_switch('/add'), all_data).then(() => {
-                    updateUI()
+                consolidate(all_data, c_data, city).then(() => {
+                    postData(backend_callback_url_switch('/add'), all_data).then(() => {
+                        c_data = {}
+                        all_data = {}
+                        updateUI()
+                    })
                 })
             })
 
@@ -19,19 +24,40 @@ function magic() {
     })
 };
 
+const consolidate = async(data1, data2, city) => {
+    data1[city] = data2;
+}
 const updateUI = async() => {
-    // let d = new Date();
-    // let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-    const country = document.getElementById('country');
-    const temp = document.getElementById('temp');
-    const content = document.getElementById('content');
     const travelday = document.getElementById('travelday');
     const request = await fetch(backend_callback_url_switch('/all'));
 
     try {
         const data = await request.json();
+        const trips_section = document.getElementById('trips')
+        for (let acity of Object.keys(data)) {
+
+            let trip = `<div class='trip_card' id='card'>
+                        <div id="city_img">
+                        <img src="${data[acity].pixabay_image}" alt="${acity}">
+                        </div>
+                        <div id='basic_info'>
+                            <div id='city'><span>City: </span>${acity}</div>
+                            <div id='Country'><span>Country: </span>${data[acity].geoname.countryName}</div>
+                            <div id='long'><span>Longitude: </span>${data[acity].geoname.lng}</div>
+                            <div id='lat'><span>Latitude: </span>${data[acity].geoname.lat}</div>
+                        </div>
+                        <div id='weather_info'>
+                        <h2>Weather</h2>
+                            <div id='day1'>
+                                <div id='description'><span>Description: </span>${data[acity].weather_bit_data.weather.description}</div>
+                                <div id='temp'><span>Temprature: </span>${data[acity].weather_bit_data.temp}</div>
+                            </div>
+                        </div>
+                    </div>`
+            trips_section.insertAdjacentHTML('beforeend', trip);
+        }
     } catch (e) {
-        console.log('erroe', e);
+        document.getElementById('error').innerHTML = 'Sorry some error occured'
     }
 };
 const getdata = async(baseurl) => {
@@ -48,8 +74,7 @@ const geoname = async(query) => {
     const baseurl = `http://api.geonames.org/searchJSON?q=${query}&maxRows=1&username=sandeep2194`;
     try {
         await getdata(baseurl).then((data) => {
-            all_data[query + '_geonames'] = data.geonames[0]
-
+            c_data['geoname'] = data.geonames[0]
         })
     } catch (e) {
         all_data[query] = {
@@ -67,7 +92,8 @@ const weatherbit = async(city) => {
     const baseurl = `http://api.weatherbit.io/v2.0/forecast/daily/?days=1&city=${city}&key=${weather_bit_api_key}`;
     try {
         await getdata(baseurl).then((data) => {
-            all_data[city + '_weather_bit_data'] = data.data[0]
+            c_data['weather_bit_data'] = data.data[0]
+
         })
     } catch (e) {
         all_data[city] = {
@@ -85,7 +111,7 @@ const pixabay = async(city) => {
     const baseurl = `https://pixabay.com/api/?key=${pixabay_api_key}&q=${city}`;
     try {
         await getdata(baseurl).then((data) => {
-            all_data[city + '_pixabay_image'] = data.hits[0].webformatURL
+            c_data['pixabay_image'] = data.hits[0].webformatURL
         })
     } catch (e) {
         all_data[city] = {
